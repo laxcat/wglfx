538d79c4ebe6f9152698e2084839dcadbb85e2a4
Tyler Martin <tyler@tylermartin.net>
2024-08-06 22:06:40 -0600

    wasm test

---
+++ 01_DEVELOP/wasm/test.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+// void console_log(uint32_t value);
+// uint8_t mcmory;
+
+// static void print(uint32_t value) {
+//     console_log(value);
+// }
+
+int add(int a, int b) {
+    return a + b;
+}
